2252e97628fc0064d22a73fd6531eacd88e2725a
Ștefan MURARU <muraru.stefaan@gmail.com>
2024-07-09 10:53:03 +0300

    feat: update to use ssl

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM golang:1.22 AS api_build
 
 WORKDIR /opt/src
-COPY ./src/go .
+COPY ./go .
 
 RUN go get -d -v ./...
 RUN go build -o api
@@ -12,11 +12,11 @@ FROM node:20-alpine AS frontend_build
 WORKDIR /app
 ENV PATH /app/node_modules/.bin:$PATH
 
-COPY ./src/frontend/.npmrc ./
-COPY ./src/frontend/package*.json ./
+COPY ./frontend/.npmrc ./
+COPY ./frontend/package*.json ./
 RUN npm install
 
-COPY ./src/frontend ./
+COPY ./frontend ./
 RUN npm run build
 
 CMD ["npm", "start"]
@@ -29,9 +29,7 @@ COPY --from=frontend_build /app/build /usr/share/nginx/html
 COPY --from=api_build /opt/src/api /app/api
 
 COPY --chmod=0755 entrypoint.sh /entrypoint.sh
-RUN apk add libc6-compat
+RUN apk add libc6-compat openssl
 
-ENV NGINX_PORT=3000
-EXPOSE 3000
-
+EXPOSE 443
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-ls -alh /app
+# Set certificate and key file paths
+CERT_FILE="/etc/nginx/ssl/nginx.crt"
+KEY_FILE="/etc/nginx/ssl/nginx.key"
+
+# Check if certificate and key files exist
+if [ ! -f "$CERT_FILE" ] || [ ! -f "$KEY_FILE" ]; then
+  echo "Certificates not found, generating self-signed certificates..."
+  mkdir -p /etc/nginx/ssl
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$KEY_FILE" -out "$CERT_FILE" -subj "/C=US/ST=State/L=City/O=Organization/OU=OrgUnit/CN=localhost" > /dev/null 2>&1
+  echo "Self-signed certificates generated."
+else
+  echo "Using existing certificates."
+fi
+
+echo You can use your own certificates by mounting them to /etc/nginx/ssl/nginx.crt and /etc/nginx/ssl/nginx.key
 
 echo "Starting Plaid Link api server"
-/app/api &
+/app/api > /dev/null &
 p1=$!
 
 echo "Starting Plaid Link frontend server"
-nginx -g "daemon off;" &
+nginx -g "daemon off;" > /dev/null &
 p2=$!
 
-echo "Services started, visit http://localhost:3000 to use Plaid Link."
+echo "Services started, visit https://localhost to use Plaid Link."
 echo "Press Ctrl+C to stop the services."
 wait $p1 $p2
